6dc3788a26e56cb85fb2a80d85368c0a536ff0ee
samadovbb <samadovbb@mail.ru>
2022-02-02 21:48:20 +0500

    avia tickets ready

--- js/datepicker.js
+++ js/datepicker.js
@@ -1,7 +1,30 @@
 const date = new Date();
 let date2 = date;
-const renderCalendar = () => {
-
+const week=[
+  "Dush",
+  "Sehs",
+  "Chor",
+  "Pay",
+  "Jum",
+  "Shan",
+  "Yak"
+], 
+months=[
+  "Yan",
+  "Fev",
+  "Mar",
+  "Apr",
+  "May",
+  "Iyn",
+  "Iyl",
+  "Avg",
+  "Sen",
+  "Okt",
+  "Noy",
+  "Dek"
+]
+function renderCalendar(target){
+  const return_element = target.querySelector(".selected-value")
   const monthDays = document.querySelector(".days");
   const lastDay = new Date(
     date.getFullYear(),
@@ -26,7 +49,6 @@ const renderCalendar = () => {
     date.getMonth() + 1,
     0
   ).getDay();
-    //console.log(lastDayIndex);
 
   const nextDays = 7 - (lastDayIndex==6?0:lastDayIndex) - 1;
 
@@ -45,31 +67,31 @@ const renderCalendar = () => {
       days += `<div>${i}</div>`;
     }
   }
-  
+  if (lastDayIndex!=6)
   for (let j = 1; j <= nextDays; j++) {
-    if (lastDayIndex!=6) days += `<div class="next-date">${j}</div>`;
-    monthDays.innerHTML = days;
+     days += `<div class="next-date">${j}</div>`;
+    
   }
+  monthDays.innerHTML = days;
   document.querySelectorAll(".days div").forEach(function(item){
     item.style.height=item.offsetWidth+"px";
     item.addEventListener("click", (event) => {
       date.setDate(event.target.innerHTML);
-      //console.log(date);
-      renderCalendar();
+      return_element.innerText=date.getDate()+" "+months[date.getMonth()]+", "+week[date.getDay()];
+      renderCalendar(target);
     });
   });
 };
-
-document.querySelector(".month1").addEventListener("change", (event) => {
-  date.setMonth(event.target.value-1);
+function create_picker(target){
+  document.querySelector(".month").addEventListener("change", function(event){
+    date.setMonth(event.target.value-1);
+    renderCalendar(target);
+  });
   
-  renderCalendar();
-  // console.clear();
-});
-
-document.querySelector(".year").addEventListener("change", (event) => {
-  date.setFullYear(event.target.value);
-  renderCalendar();
-});
-
-renderCalendar();
+  document.querySelector(".year").addEventListener("change", function(event){
+    date.setFullYear(event.target.value);
+    renderCalendar(target);
+  });
+  
+  renderCalendar(target);
+}
